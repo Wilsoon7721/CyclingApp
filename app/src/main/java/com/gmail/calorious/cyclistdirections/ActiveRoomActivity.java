f7cfb224f336849ccd1f78ba8c27db081948688b
Wilsoon <wilsonoon2018@gmail.com>
2022-04-11 07:55:21 +0800

    .

--- app/src/main/java/com/gmail/calorious/cyclistdirections/ActiveRoomActivity.java
+++ app/src/main/java/com/gmail/calorious/cyclistdirections/ActiveRoomActivity.java
@@ -8,12 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.gmail.calorious.cyclistdirections.db.RoomDatabase;
 
 public class ActiveRoomActivity extends AppCompatActivity {
-
+        // Chances are its gonna waste a shit ton of data, so u can try to optimise it 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Retrieve room
-        // If Room is a new room, create a new room database and pass to RoomManager handler together with the room id
+        // If Room is a new room (find that out from the database query (room id), create a new room database and pass to RoomManager handler together with the room id
         // If it's an existing room then grab the room data from the RoomManager
     }
 }
--- app/src/main/java/com/gmail/calorious/cyclistdirections/HomeScreenActivity.java
+++ app/src/main/java/com/gmail/calorious/cyclistdirections/HomeScreenActivity.java
@@ -1,16 +1,17 @@
 package com.gmail.calorious.cyclistdirections;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.gmail.calorious.cyclistdirections.db.RoomDatabase;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class HomeScreenActivity extends AppCompatActivity {
-
+    private EditText codeField;
+    private Button createButton, joinButton;
     /*
     ANDROID ROOM DATABASES WONT WORK - REQUIRES A SHARED STORAGE
      */
@@ -19,5 +20,21 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.home_screen);
+        codeField = findViewById(R.id.insert_code_field);
+        createButton = findViewById(R.id.create_button);
+        joinButton = findViewById(R.id.join_code_button);
+    }
+
+    public void onButtonPressed(View view) {
+        if(view.getId() == R.id.create_button) {
+            // Create room?
+            return;
+        }
+        if(view.getId() == R.id.join_code_button) {
+            String baseText = codeField.getText().toString();
+            if(baseText.length() < 6 || baseText.length() > 6) {
+                
+            }
+        }
     }
 }
