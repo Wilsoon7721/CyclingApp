b4d99a4f29c25fd5b5c30daa0ebe7b91803899b1
Wilsoon <wilsonoon2018@gmail.com>
2022-04-14 10:31:23 +0800

    f
    f

--- app/src/main/java/com/gmail/calorious/cyclistdirections/general/CyclistRoom.java
+++ app/src/main/java/com/gmail/calorious/cyclistdirections/general/CyclistRoom.java
@@ -1,8 +1,12 @@
 package com.gmail.calorious.cyclistdirections.general;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CyclistRoom {
     /*
      * General class for an active room
      */
-    public static void
+    private long id;
+    private Set<User> activeUsers = new HashSet<>();
 }
