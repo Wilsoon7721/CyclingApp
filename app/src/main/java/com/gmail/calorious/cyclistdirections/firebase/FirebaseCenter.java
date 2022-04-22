04cde63a1e5f008a0dfc9a8229141030c9ed044a
Wilsoon <wilsonoon2018@gmail.com>
2022-04-22 13:48:04 +0800

    f

---
+++ app/src/main/java/com/gmail/calorious/cyclistdirections/firebase/FirebaseCenter.java
@@ -0,0 +1,14 @@
+package com.gmail.calorious.cyclistdirections.firebase;
+
+import android.content.Context;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseCenter {
+    // Purpose of class is to handle firebase requests
+    public void initialize(Context context) {
+        FirebaseApp.initializeApp(context);
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://cyclistdirections-default-rtdb.asia-southeast1.firebasedatabase.app");
+    }
+}
