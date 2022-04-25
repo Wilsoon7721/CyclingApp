b79c560ea23ccfc989c988722275e901a1ad2883
Wilsoon <wilsonoon2018@gmail.com>
2022-04-25 09:14:59 +0800

    Update FirebaseCenter.java

--- app/src/main/java/com/gmail/calorious/cyclistdirections/firebase/FirebaseCenter.java
+++ app/src/main/java/com/gmail/calorious/cyclistdirections/firebase/FirebaseCenter.java
@@ -10,5 +10,6 @@ public class FirebaseCenter {
     public void initialize(Context context) {
         FirebaseApp.initializeApp(context);
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://cyclistdirections-default-rtdb.asia-southeast1.firebasedatabase.app");
+        
     }
 }
